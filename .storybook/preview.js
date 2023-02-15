90dbaf0d5b03e7f25e0834ed2ab74f01c376d9d4
asifulhaque087 <asifulhaque087@gmail.com>
2023-02-15 14:32:26 +0600

    integret tailwind into storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+/**
+ * This is necessary to make tailwind work in storybook
+ */
+import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -14,7 +14,7 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <div className='p-52 bg-red-500'><Button {...args} /></div>;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
